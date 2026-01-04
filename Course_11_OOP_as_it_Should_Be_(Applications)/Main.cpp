0552ef07833400ea05051d373b11546f9e0dfcf5
zakariya <zakariasabri2022@gmail.com>
2026-01-04 14:28:24 +0100

    add the Main.cpp before add the Extension

--- Course_11_OOP_as_it_Should_Be_(Applications)/Main.cpp
+++ Course_11_OOP_as_it_Should_Be_(Applications)/Main.cpp
@@ -1,77 +1,28 @@
-#include <iostream>
-#include "clsBankClient.h"
-#include "clsInputValidate.h"
-
-void ReadClientInfo(clsBankClient& Client)
+// system is a function that alwo to you code execute a OS commands , the process is :
+//  Your C++ program → Calls OS → OS executes the command → Returns to your program
+//  like : system("cls");
+// also it depande on your machine
+#include "clsLoginScreen.h"
+void testD_affichage()
 {
-    cout << "\nEnter FirstName: ";
-    Client.SetFirstName( clsInputValidate::ReadString());
-
-    cout << "\nEnter LastName: ";
-    Client.SetLastName(clsInputValidate::ReadString());
-
-    cout << "\nEnter Email: ";
-    Client.SetEmail  (clsInputValidate::ReadString());
-
-    cout << "\nEnter Phone: ";
-    Client.SetPhone(clsInputValidate::ReadString());
-
-    cout << "\nEnter PinCode: ";
-    Client.SetPinCode  (clsInputValidate::ReadString());
-
-    cout << "\nEnter Account Balance: ";
-    Client.SetAccountBalance(clsInputValidate::ReadFloatNumber());
+    cout << "-------------------------------------------------------" << endl;
+    cout << "the curent  user name   : " << CurrentUser.GetUserName() << endl;
+    cout << "the curent  first name  : " << CurrentUser.GetFirstName() << endl;
+    cout << "the curent  last name   : " << CurrentUser.GetLastName() << endl;
+    cout << "the curent  Password    : " << CurrentUser.GetPassword() << endl;
+    cout << "the curent  Permissions : " << CurrentUser.GetPermissions() << endl;
+    cout << "-------------------------------------------------------" << endl;
 }
-
-void UpdateClient()
-{
-    string AccountNumber = "";
-
-    cout << "\nPlease Enter client Account Number: ";
-    AccountNumber = clsInputValidate::ReadString();
-    
-    while (!clsBankClient::IsClientExist(AccountNumber))
-    {
-        cout << "\nAccount number is not found, choose another one: ";
-        AccountNumber = clsInputValidate::ReadString();
-    }
-
-    clsBankClient Client1 = clsBankClient::Find(AccountNumber);
-    Client1.Print();
-
-    cout << "\n\nUpdate Client Info:";
-    cout << "\n____________________\n";
-
-  
-    ReadClientInfo(Client1);
-
-    clsBankClient::enSaveResults SaveResult;
-
-    SaveResult = Client1.Save();
-
-    switch (SaveResult)
-    {
-    case  clsBankClient::enSaveResults::svSucceeded:
-    {
-        cout << "\nAccount Updated Successfully :-)\n";
-        Client1.Print();
-        break;
-    }
-    case clsBankClient::enSaveResults::svFaildEmptyObject:
-    {
-        cout << "\nError account was not saved because it's Empty";
-        break;
-
-    }
-
-    }
-}
-
 int main()
 
 {
-    UpdateClient();
+    // clsMainScreen::ShowMainMenue();
+
+    while ((4-2)%2==0/*don't use true hhh*/)
+    {
+        clsLoginScreen::ShowLoginScreen();
+    }
+    // testD_affichage();
     system("pause>0");
     return 0;
-    
 }
